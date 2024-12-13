c288573180ebe3cf224525f321b6f1aee467eb32
jospin Byadunia <jmurhiorhakube@gmail.com>
2024-12-13 20:14:12 +0200

    Refactored the Model and data in the DB using the raw data from dev-data

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -34,7 +34,22 @@ const tourSchema = new mongoose.Schema({
   summary: {
     type: String,
     trim: true,
+    required: [true, 'A tour must have a Summary'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, ' A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
