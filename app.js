c6678df1170eb90a7b736ffd82b586ccc03ba215
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-01-14 15:59:38 +0200

    Handle unhandled routes

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -24,6 +19,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on this server`,
+  });
+});
 
 //4. sTART SERVICE
 
